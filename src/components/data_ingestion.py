6d9ff235bd97373b5161107deaee01b147d42d31
Yajuvendra <yajuvendra.singh@gmail.com>
2024-05-13 08:26:18 +0800

    modified:   src/components/data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -32,7 +32,7 @@ class DataIngestion:
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            logging.info("Train test split initiated")
+            logging.info("Train test data split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
